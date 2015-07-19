328cad68427443f66336bf4717b6c2e00fd4a5cd
iiwaasnet <givasena@gmail.com>
2015-07-19 11:11:07 +0200

    Unittesting

---
+++ src/rawf.Tests/Messaging/MessageTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NUnit.Framework;
+using rawf.Messaging;
+using rawf.Tests.Actor.Setup;
+
+namespace rawf.Tests.Messaging
+{
+    [TestFixture]
+    public class MessageTests
+    {
+        [Test]
+        public void TestFlowStartMessage_HasCorrelationIdSet()
+        {
+            var message = Message.CreateFlowStartMessage(new SimpleMessage(), SimpleMessage.MessageIdentity);
+
+            Assert.IsNotNull(message.CorrelationId);
+            CollectionAssert.IsNotEmpty(message.CorrelationId);
+        }
+
+        [Test]
+        public void TestMessage_HasVersionSet()
+        {
+            var message = Message.CreateFlowStartMessage(new SimpleMessage(), SimpleMessage.MessageIdentity);
+
+            Assert.IsNotNull(message.Version);
+            CollectionAssert.AreEqual(Message.CurrentVersion, message.Version);
+        }
+
+        [Test]
+        public void TestMessage_HasIdentitySet()
+        {
+            var message = Message.CreateFlowStartMessage(new SimpleMessage(), SimpleMessage.MessageIdentity);
+
+            Assert.IsNotNull(message.Identity);
+            CollectionAssert.AreEqual(SimpleMessage.MessageIdentity, message.Identity);
+        }
+
+        [Test]
+        public void TestMessage_HasDistributionPatternSet()
+        {
+            var message = Message.CreateFlowStartMessage(new SimpleMessage(), SimpleMessage.MessageIdentity);
+
+            CollectionAssert.Contains(Enum.GetValues(typeof(DistributionPattern)), message.Distribution);
+        }
+    }
+}
