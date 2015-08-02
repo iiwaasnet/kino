f267e80b494a497dfd98a6ebc06e38e1020d9a58
iiwaasnet <givasena@gmail.com>
2015-08-02 15:00:43 +0200

    Refactoring

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,16 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Autofac;
 using rawf.Client;
 
 namespace Client
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             var builder = new ContainerBuilder();
             builder.RegisterModule(new MainModule());
