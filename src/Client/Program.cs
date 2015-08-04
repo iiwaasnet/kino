c1e8745f914d6aca884ec492399f58f90131a074
iiwaasnet <givasena@gmail.com>
2015-08-04 22:15:07 +0200

    Adding performance test

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-using System.Threading;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using Autofac;
 using Client.Messages;
 using rawf.Client;
@@ -23,20 +25,53 @@ namespace Client
             var messageHub = container.Resolve<IMessageHub>();
             messageHub.Start();
 
-            Thread.Sleep(TimeSpan.FromSeconds(20));
-            
             Console.WriteLine("Client is running...");
 
-            var message = Message.CreateFlowStartMessage(new HelloMessage { Greeting = "Hello world!" }, HelloMessage.MessageIdentity);
-            var callback = new CallbackPoint(EhlloMessage.MessageIdentity);
-            var promise = messageHub.EnqueueRequest(message, callback);
-            var resp = promise.GetResponse().Result.GetPayload<EhlloMessage>();
-            Console.WriteLine(resp.Ehllo);
-            
+            //var message = Message.CreateFlowStartMessage(new HelloMessage {Greeting = "Hello world!"}, HelloMessage.MessageIdentity);
+            //var callback = new CallbackPoint(EhlloMessage.MessageIdentity);
+            //var promise = messageHub.EnqueueRequest(message, callback);
+            //var resp = promise.GetResponse().Result.GetPayload<EhlloMessage>();
+            //Console.WriteLine(resp.Ehllo);
+
+            RunTest(messageHub, 100000);
+
             Console.ReadLine();
             messageHub.Stop();
             messageRouter.Stop();
             ccMon.Stop();
+        }
+
+        private static void RunTest(IMessageHub messageHub, int runs)
+        {
+            var callbackPoint = new CallbackPoint(EhlloMessage.MessageIdentity);
+
+            var timer = new Stopwatch();
+            timer.Start();
+
+            var responses = new List<Task<IMessage>>();
+            for (var i = 0; i < runs; i++)
+            {
+                var message = Message.CreateFlowStartMessage(new HelloMessage {Greeting = "Hello"}, HelloMessage.MessageIdentity);
+                responses.Add(messageHub.EnqueueRequest(message, callbackPoint).GetResponse());
+            }
+
+            responses.ForEach(r =>
+                              {
+                                  try
+                                  {
+                                      r.Wait();
+                                      var msg = r.Result.GetPayload<EhlloMessage>();
+                                      //System.Console.WriteLine($"Received: {msg.Ehllo}");
+                                  }
+                                  catch (Exception err)
+                                  {
+                                      Console.WriteLine($"Error happened: {err.ToString()}");
+                                  }
+                              });
+
+            timer.Stop();
+
+            Console.WriteLine($"Done {runs} times in {timer.ElapsedMilliseconds} msec");
         }
     }
 }
