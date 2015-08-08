052fe8936d70723a633bdc73431b07c4497b9c0c
iiwaasnet <givasena@gmail.com>
2015-08-08 15:24:00 +0200

    Ping/Pong

--- src/rawf.Rendezvous/ApplicationConfiguration.cs
+++ src/rawf.Rendezvous/ApplicationConfiguration.cs
@@ -1,8 +1,11 @@
-﻿namespace rawf.Rendezvous
+﻿using System;
+
+namespace rawf.Rendezvous
 {
     public class ApplicationConfiguration
     {
         public string BroadcastUri { get; set; }
         public string UnicastUri { get; set; }
+        public TimeSpan PingInterval { get; set; }
     }
 }
--- src/rawf.Rendezvous/RendezvousService.cs
+++ src/rawf.Rendezvous/RendezvousService.cs
@@ -1,7 +1,8 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
-using rawf.Framework;
+using rawf.Messaging;
+using rawf.Messaging.Messages;
 using rawf.Sockets;
 using TypedConfigProvider;
 
@@ -12,6 +13,7 @@ namespace rawf.Rendezvous
         private readonly ISocketFactory socketFactory;
         private readonly CancellationTokenSource cancellationTokenSource;
         private Task messageProcessing;
+        private Task pinging;
         private readonly ApplicationConfiguration config;
 
         public RendezvousService(ISocketFactory socketFactory, IConfigProvider configProvider)
@@ -23,15 +25,48 @@ namespace rawf.Rendezvous
 
         public void Start()
         {
-            const int participantCount = 2;
+            const int participantCount = 3;
             using (var gateway = new Barrier(participantCount))
             {
                 messageProcessing = Task.Factory.StartNew(_ => ProcessMessages(cancellationTokenSource.Token, gateway),
+                                                          TaskCreationOptions.LongRunning);
+                pinging = Task.Factory.StartNew(_ => PingClusterMembers(cancellationTokenSource.Token, gateway),
                                                           TaskCreationOptions.LongRunning);
 
                 gateway.SignalAndWait(cancellationTokenSource.Token);
             }
         }
+
+        private void PingClusterMembers(CancellationToken token, Barrier gateway)
+        {
+            try
+            {
+                using (var wait = new ManualResetEventSlim(false))
+                {
+                    using (var pingNotificationSocket = CreatePingNotificationSocket())
+                    {
+                        gateway.SignalAndWait(token);
+
+                        while (!token.IsCancellationRequested)
+                        {
+                            var message = Message.Create(new PingMessage(), PingMessage.MessageIdentity);
+                            pingNotificationSocket.SendMessage(message);
+
+                            wait.Wait(config.PingInterval, token);
+                        }
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine(err);
+            }
+        }
+
+        
 
         private void ProcessMessages(CancellationToken token, Barrier gateway)
         {
@@ -78,6 +113,13 @@ namespace rawf.Rendezvous
             return socket;
         }
 
+        private ISocket CreatePingNotificationSocket()
+        {
+            var socket = socketFactory.CreateDealerSocket();
+            socket.Connect(new Uri(config.UnicastUri));
+
+            return socket;
+        }
         public void Stop()
         {
             cancellationTokenSource.Cancel(true);
