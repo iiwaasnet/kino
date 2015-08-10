a9f6214d74d5080891beddca50f430347b0832bc
iiwaasnet <givasena@gmail.com>
2015-08-10 12:36:38 +0200

    Reconnection

--- src/rawf/Connectivity/ExternalRoutingTable.cs
+++ src/rawf/Connectivity/ExternalRoutingTable.cs
@@ -7,20 +7,22 @@ namespace rawf.Connectivity
 {
     public class ExternalRoutingTable : IExternalRoutingTable
     {
-        private readonly IDictionary<MessageHandlerIdentifier, HashSet<SocketIdentifier>> messageHandlersMap;
+        private readonly IDictionary<MessageHandlerIdentifier, IDictionary<SocketIdentifier, ISocket>> messageHandlersMap;
         private readonly IDictionary<SocketIdentifier, HashSet<MessageHandlerIdentifier>> socketToMessageMap;
         private readonly IDictionary<SocketIdentifier, Uri> socketToUriMap;
+        private readonly ISocketFactory socketFactory;
 
-        public ExternalRoutingTable()
+        public ExternalRoutingTable(ISocketFactory socketFactory)
         {
-            messageHandlersMap = new Dictionary<MessageHandlerIdentifier, HashSet<SocketIdentifier>>();
+            messageHandlersMap = new Dictionary<MessageHandlerIdentifier, IDictionary<SocketIdentifier, ISocket>>();
             socketToMessageMap = new Dictionary<SocketIdentifier, HashSet<MessageHandlerIdentifier>>();
             socketToUriMap = new Dictionary<SocketIdentifier, Uri>();
+            this.scoketFactory = socketFactory;
         }
 
         public void Push(MessageHandlerIdentifier messageHandlerIdentifier, SocketIdentifier socketIdentifier, Uri uri)
         {
-            var mapped = MapMessageToSocket(messageHandlerIdentifier, socketIdentifier);
+            var mapped = MapMessageToSocket(messageHandlerIdentifier, socketIdentifier, Uri uri);
 
             if (mapped)
             {
@@ -32,15 +34,24 @@ namespace rawf.Connectivity
             }
         }
 
-        private bool MapMessageToSocket(MessageHandlerIdentifier messageHandlerIdentifier, SocketIdentifier socketIdentifier)
+        private bool MapMessageToSocket(MessageHandlerIdentifier messageHandlerIdentifier, SocketIdentifier socketIdentifier, Uri uri)
         {
-            HashSet<SocketIdentifier> hashSet;
-            if (!messageHandlersMap.TryGetValue(messageHandlerIdentifier, out hashSet))
+            IDictionary<SocketIdentifier, ISocket> sockets;
+            if (!messageHandlersMap.TryGetValue(messageHandlerIdentifier, out sockets))
             {
-                hashSet = new HashSet<SocketIdentifier>();
-                messageHandlersMap[messageHandlerIdentifier] = hashSet;
+                sockets = new Dictionary<SocketIdentifier, ISocket>();
+                messageHandlersMap[messageHandlerIdentifier] = sockets;
             }
-            return hashSet.Add(socketIdentifier);
+            if (!sockets.ContainsKey(socketIdentifier))
+            {
+                var socket = socketFactory.CreateDealerSocket();
+                socket.Connect(uri);
+                sockets[socketIdentifier] = socket; 
+                
+                return true;
+            }
+            
+            return false;
         }
 
         private void MapSocketToMessage(MessageHandlerIdentifier messageHandlerIdentifier, SocketIdentifier socketIdentifier)
@@ -57,31 +68,45 @@ namespace rawf.Connectivity
         public SocketIdentifier Pop(MessageHandlerIdentifier messageHandlerIdentifier)
         {
             //TODO: Implement round robin
-            HashSet<SocketIdentifier> collection;
-            return messageHandlersMap.TryGetValue(messageHandlerIdentifier, out collection)
-                       ? Get(collection)
+            IDictionary<SocketIdentifier, ISocket> sockets;
+            return messageHandlersMap.TryGetValue(messageHandlerIdentifier, out csocketsollection)
+                       ? Get(sockets)
                        : null;
         }
 
-        private static T Get<T>(ICollection<T> hashSet)
-            => hashSet.Any()
-                   ? hashSet.First()
-                   : default(T);
+        private static V Get<V>(IDictionary<K, V> collection)
+            => collection.Any()
+                   ? collection.Values.First()
+                   : default(V);
 
         public void RemoveRoute(SocketIdentifier socketIdentifier)
         {
-            socketToUriMap.Remove(socketIdentifier);
+            Uri uri;
+            if(socketToUriMap.TryGetValue(socketIdentifier, out uri))
+            {
+                socketToUriMap.Remove(socketIdentifier);
+            }
+            
 
             HashSet<MessageHandlerIdentifier> messageHandlers;
             if (socketToMessageMap.TryGetValue(socketIdentifier, out messageHandlers))
             {
                 foreach (var messageHandlerIdentifier in messageHandlers)
                 {
-                    HashSet<SocketIdentifier> socketIdentifiers;
+                    IDictionary<SocketIdentifier, ISocket> sockets;
                     if (messageHandlersMap.TryGetValue(messageHandlerIdentifier, out socketIdentifiers))
                     {
-                        socketIdentifiers.Remove(socketIdentifier);
-                        if (!socketIdentifiers.Any())
+                        ISocket socket;
+                        if (sockets.TryGetValue(socketIdentifier, out socket))
+                        {
+                            sockets.Remove(socketIdentifier);
+                            if (uri != null)
+                            {
+                                socket.Disconnect(uri);
+                            }
+                            socket.Dispose();
+                        }
+                        if (!sockets.Any())
                         {
                             messageHandlersMap.Remove(messageHandlerIdentifier);
                         }
