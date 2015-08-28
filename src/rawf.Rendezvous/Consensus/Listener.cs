5d8efa548c5e388551e4cc72f303509b8822915c
iiwaasnet <givasena@gmail.com>
2015-08-28 22:22:05 +0200

    Removed unused code

--- src/rawf.Rendezvous/Consensus/Listener.cs
+++ src/rawf.Rendezvous/Consensus/Listener.cs
@@ -8,20 +8,14 @@ namespace rawf.Rendezvous.Consensus
     public class Listener : IListener
     {
         private readonly ConcurrentDictionary<IObserver<IMessage>, object> observers;
-        //private readonly BlockingCollection<IMessage> messages;
-        //private Action<IMessage> appendMessage;
         private readonly Action<Listener> unsubscribe;
         private readonly ILogger logger;
 
         public Listener(Action<Listener> unsubscribe, ILogger logger)
         {
             observers = new ConcurrentDictionary<IObserver<IMessage>, object>();
-            //messages = new BlockingCollection<IMessage>(new ConcurrentQueue<IMessage>());
-            //appendMessage = DropMessage;
             this.unsubscribe = unsubscribe;
             this.logger = logger;
-            ////TODO: might be quite expensive to spawn every time new thread
-            //new Thread(ForwardMessages).Start();
         }
 
         public void Notify(IMessage message)
@@ -39,34 +33,6 @@ namespace rawf.Rendezvous.Consensus
             }
         }
 
-        //private void AddMessageToQueue(IMessage message)
-        //{
-        //    messages.Add(message);
-        //}
-
-        //private void DropMessage(IMessage message)
-        //{
-        //}
-
-        //private void ForwardMessages()
-        //{
-        //    foreach (var message in messages.GetConsumingEnumerable())
-        //    {
-        //        foreach (var observer in observers.Keys)
-        //        {
-        //            try
-        //            {
-        //                observer.OnNext(message);
-        //            }
-        //            catch (Exception err)
-        //            {
-        //                logger.Error(err);
-        //            }
-        //        }
-        //    }
-        //    messages.Dispose();
-        //}
-
         public IDisposable Subscribe(IObserver<IMessage> observer)
         {
             observers[observer] = null;
@@ -74,21 +40,9 @@ namespace rawf.Rendezvous.Consensus
             return new Unsubscriber(observers, observer);
         }
 
-        //public void Start()
-        //{
-        //    Interlocked.Exchange(ref appendMessage, AddMessageToQueue);
-        //}
-
-        //public void Stop()
-        //{
-        //    Interlocked.Exchange(ref appendMessage, DropMessage);
-        //}
-
         public void Dispose()
         {
             unsubscribe(this);
-            //Stop();
-            //messages.CompleteAdding();
         }
 
         private class Unsubscriber : IDisposable
