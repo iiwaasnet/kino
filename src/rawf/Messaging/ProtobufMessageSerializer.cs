9534509e1f85dfd1477254af4a1339d135582499
iiwaasnet <givasena@gmail.com>
2015-08-28 19:24:47 +0200

    Removed commented code

--- src/rawf/Messaging/ProtobufMessageSerializer.cs
+++ src/rawf/Messaging/ProtobufMessageSerializer.cs
@@ -5,14 +5,6 @@ namespace rawf.Messaging
 {
     public class ProtobufMessageSerializer : IMessageSerializer
     {
-        static ProtobufMessageSerializer()
-        {
-            //var exception = RuntimeTypeModel.Default.Add(typeof (Exception), false);
-            //exception.AddField(1, "Message");
-            //exception.AddSubType(1, typeof (AggregateException));
-            //RuntimeTypeModel.Default.Add(typeof (AggregateException), true);
-        }
-
         public byte[] Serialize(object obj)
         {
             using (var stream = new MemoryStream())
