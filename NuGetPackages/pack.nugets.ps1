ffbb48ae3bc73822b5f6096037bae4b8de977314
iiwaasnet <givasena@gmail.com>
2015-09-06 17:54:24 +0200

    Changed to MSBuild 14.0

--- NuGetPackages/pack.nugets.ps1
+++ NuGetPackages/pack.nugets.ps1
@@ -1,14 +1,15 @@
-$MsBuild = $env:SystemRoot + "\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
+$MsBuild = "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe"
 
 foreach ($NugetSpec in Get-ChildItem ..\src -Recurse | Where-Object {$_.Extension -eq ".nuspec"})
 {
 	$ProjectFile = Get-ChildItem $NugetSpec.DirectoryName | Where-Object {$_.Extension -eq ".csproj"}
+
 	$BuildArgs = @{
 		FilePath = $MsBuild
-		ArgumentList = $ProjectFile.FullName,  "/p:Configuration=Release"
+		ArgumentList = $ProjectFile.FullName,  "/p:Configuration=Release /t:Rebuild"
 		Wait = $true
 	}
 	
-	## Start-Process @BuildArgs -NoNewWindow
+	Start-Process @BuildArgs -NoNewWindow
 	nuget pack $NugetSpec.FullName -Build -NonInteractive
 }
