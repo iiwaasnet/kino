0eb2a4d1ae19bce23f4b55fd9285f1bbb5146fd3
iiwaasnet <givasena@gmail.com>
2015-09-06 17:24:46 +0200

    Aligning NuGet packages

--- NuGetPackages/pack.nugets.ps1
+++ NuGetPackages/pack.nugets.ps1
@@ -1,4 +1,14 @@
-foreach ($file in Get-ChildItem ..\src -Recurse | Where-Object {$_.Extension -eq ".nuspec"})
+$MsBuild = $env:SystemRoot + "\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe"
+
+foreach ($NugetSpec in Get-ChildItem ..\src -Recurse | Where-Object {$_.Extension -eq ".nuspec"})
 {
-	nuget pack $file.FullName -Build -NonInteractive
+	$ProjectFile = Get-ChildItem $NugetSpec.DirectoryName | Where-Object {$_.Extension -eq ".csproj"}
+	$BuildArgs = @{
+		FilePath = $MsBuild
+		ArgumentList = $ProjectFile.FullName,  "/p:Configuration=Release"
+		Wait = $true
+	}
+	
+	## Start-Process @BuildArgs -NoNewWindow
+	nuget pack $NugetSpec.FullName -Build -NonInteractive
 }
