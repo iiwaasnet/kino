9b34287825e71367ce2ee1c722c5163bb6376f99
iiwaasnet <givasena@gmail.com>
2015-09-08 22:10:43 +0200

    Added NuGet package restore

--- NuGetPackages/pack.nugets.ps1
+++ NuGetPackages/pack.nugets.ps1
@@ -1,5 +1,8 @@
 $MsBuild = "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe"
 
+$SolutionFile = Get-ChildItem ..\src -Recurse | Where-Object {$_.Extension -eq ".sln"}
+nuget restore $SolutionFile.FullName
+
 foreach ($NugetSpec in Get-ChildItem ..\src -Recurse | Where-Object {$_.Extension -eq ".nuspec"})
 {
 	$ProjectFile = Get-ChildItem $NugetSpec.DirectoryName | Where-Object {$_.Extension -eq ".csproj"}
@@ -10,6 +13,6 @@ foreach ($NugetSpec in Get-ChildItem ..\src -Recurse | Where-Object {$_.Extensio
 		Wait = $true
 	}
 	
-	Start-Process @BuildArgs
+	Start-Process @BuildArgs -NoNewWindow
 	nuget pack $NugetSpec.FullName -Build -NonInteractive
 }
