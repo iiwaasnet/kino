f87a9244e770d140b1ab34d81b63a04cbde4872f
iiwaasnet <givasena@gmail.com>
2015-10-26 17:04:20 +0100

    Refactoring

--- src/Samples/Server/Actors/RevertStringActor.cs
+++ src/Samples/Server/Actors/RevertStringActor.cs
@@ -13,23 +13,8 @@ namespace Server.Actors
         private async Task<IActorResult> StartProcess(IMessage message)
         {
             var hello = message.GetPayload<HelloMessage>();
-            //System.Console.WriteLine(hello.Greeting);
-
-            //return await Task.Delay(0)
-            //                 .ContinueWith(_ => Message.Create(new EhlloMessage
-            //                                                   {
-            //                                                       Ehllo = new string(hello.Greeting.Reverse().ToArray())
-            //                                                   },
-            //                                                   EhlloMessage.MessageIdentity))
-            //                                                   .ConfigureAwait(false);
-
-            //throw new Exception("Bla!");
-
-            //Thread.Sleep(50000);
 
             var reversedString = new string(hello.Greeting.Reverse().ToArray());
-
-            //WriteLine(reversedString);
 
             return new ActorResult(Message.Create(new EhlloMessage {Ehllo = reversedString}));
         }
