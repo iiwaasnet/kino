39eba2b0f08162f0fe44d68a1b810867774ed31c
iiwaasnet <givasena@gmail.com>
2016-02-18 19:53:03 +0100

    Added HashedQueue

---
+++ src/kino.Core/Framework/HashedQueue.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using C5;
+
+namespace kino.Core.Framework
+{
+    public class HashedQueue<T>
+    {
+        private readonly int maxQueueLength;
+        private readonly HashedLinkedList<T> collection;
+        private readonly object @lock = new object();
+
+        public HashedQueue(int maxQueueLength)
+        {
+            this.maxQueueLength = maxQueueLength;
+            collection = new HashedLinkedList<T>();
+        }
+
+        public HashedQueue()
+            :this(int.MaxValue)
+        {
+        }
+
+        public bool TryEnqueue(T item)
+        {
+            lock (@lock)
+            {
+                if (collection.Count < maxQueueLength && !collection.Contains(item))
+                {
+                    collection.InsertLast(item);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool TryDequeue(out T item)
+        {
+            item = default(T);
+
+            lock (@lock)
+            {
+                if (collection.Count > 0)
+                {
+                    item = collection.RemoveFirst();
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool TryDequeue(out IEnumerable<T> items, int count)
+        {
+            var tmp = new List<T>(count);
+            items = tmp;
+
+            lock (@lock)
+            {
+                var dequeueCount = Math.Min(collection.Count, count);
+                while (dequeueCount-- > 0)
+                {
+                    tmp.Add(collection.RemoveFirst());
+                }
+            }
+
+            return items.Any();
+        }
+    }
+}
