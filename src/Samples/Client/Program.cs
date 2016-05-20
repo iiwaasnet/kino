8872c0d144f1bbf7aa382427878821f8668ae828
iiwaasnet <givasena@gmail.com>
2016-05-20 20:04:36 +0200

    Reverted changes

--- src/Samples/Client/Program.cs
+++ src/Samples/Client/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Runtime.Remoting.Messaging;
 using System.Threading;
 using Autofac;
 using Autofac.kino;
@@ -11,7 +9,6 @@ using kino.Client;
 using kino.Core.Connectivity;
 using kino.Core.Diagnostics;
 using kino.Core.Messaging;
-using kino.Core.Messaging.Messages;
 using kino.Core.Sockets;
 using static System.Console;
 
@@ -26,68 +23,34 @@ namespace Client
             builder.RegisterModule<KinoModule>();
             var container = builder.Build();
 
-            var logger = container.Resolve<ILogger>();
             var messageRouter = container.Resolve<IMessageRouter>();
             messageRouter.Start(TimeSpan.FromSeconds(3));
             // Needed to let router bind to socket over INPROC. To be fixed by NetMQ in future.
-            //Thread.Sleep(TimeSpan.FromMilliseconds(300));
+            Thread.Sleep(TimeSpan.FromMilliseconds(300));
 
             var messageHub = container.Resolve<IMessageHub>();
             messageHub.Start();
 
             Thread.Sleep(TimeSpan.FromSeconds(5));
-            logger.Trace($"Client is running... {DateTime.Now}");
-            var runs = 10;
+            WriteLine($"Client is running... {DateTime.Now}");
 
-            var receiverIdentity = FindReceiver(messageHub);
-
-            while (true)
+            var request = Message.CreateFlowStartMessage(new HelloMessage {Greeting = Guid.NewGuid().ToString()});
+            request.TraceOptions = MessageTraceOptions.None;
+            var callbackPoint = CallbackPoint.Create<GroupCharsResponseMessage>();
+            var promise = messageHub.EnqueueRequest(request, callbackPoint);
+            if (promise.GetResponse().Wait(TimeSpan.FromSeconds(4)))
             {
-                var promises = new List<IPromise>(runs);
+                var response = promise.GetResponse().Result.GetPayload<GroupCharsResponseMessage>();
 
-                var timer = new Stopwatch();
-                timer.Start();
-
-                for (var i = 0; i < runs; i++)
+                WriteLine($"Text: {response.Text}");
+                foreach (var groupInfo in response.Groups)
                 {
-                    var request = Message.CreateFlowStartMessage(new HelloMessage {Greeting = Guid.NewGuid().ToString()});
-                    request.TraceOptions = MessageTraceOptions.None;
-                    request.SetReceiverNode(receiverIdentity);
-                    var callbackPoint = CallbackPoint.Create<GroupCharsResponseMessage>();
-                    promises.Add(messageHub.EnqueueRequest(request, callbackPoint));
+                    WriteLine($"Char: {groupInfo.Char} - {groupInfo.Count} times");
                 }
-
-                var timeout = TimeSpan.FromMilliseconds(400);
-                foreach (var promise in promises)
-                {
-                    using (promise)
-                    {
-                        if (promise.GetResponse().Wait(timeout))
-                        {
-                            promise.GetResponse().Result.GetPayload<GroupCharsResponseMessage>();
-
-                            //WriteLine($"Text: {response.Text}");
-                            //foreach (var groupInfo in response.Groups)
-                            //{
-                            //    WriteLine($"Char: {groupInfo.Char} - {groupInfo.Count} times");
-                            //}
-                        }
-                        else
-                        {
-                            logger.Warn($"{DateTime.UtcNow} Call timed out after {timeout.TotalSeconds} sec.");
-                        }
-                    }
-                }
-
-                timer.Stop();
-
-                var messagesPerTest = 3;
-                var performance = (timer.ElapsedMilliseconds > 0)
-                                      ? ((messagesPerTest * runs) / (double) timer.ElapsedMilliseconds * 1000).ToString("##.00")
-                                      : "Infinite";
-                logger.Trace($"Done {runs} times in {timer.ElapsedMilliseconds} ms with {performance} msg/sec");
-
-                Thread.Sleep(TimeSpan.FromMilliseconds(50));
+            }
+            else
+            {
+                WriteLine($"Call timed out after {TimeSpan.FromSeconds(4).TotalSeconds} sec.");
             }
 
             ReadLine();
@@ -96,20 +59,6 @@ namespace Client
             container.Dispose();
 
             WriteLine("Client stopped.");
-        }
-
-        private static SocketIdentifier FindReceiver(IMessageHub messageHub)
-        {
-            var request = Message.CreateFlowStartMessage(new RequestKnownMessageRoutesMessage());
-            var callback = CallbackPoint.Create<KnownMessageRoutesMessage>();
-            using (var promise = messageHub.EnqueueRequest(request, callback))
-            {
-                var response = promise.GetResponse().Result;
-                var registeredRoutes = response.GetPayload<KnownMessageRoutesMessage>();
-
-                return new SocketIdentifier(registeredRoutes.InternalRoutes.SocketIdentity);
-            }
-
         }
     }
 }
