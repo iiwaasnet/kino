97a55eab2576e2e23b02943f24bec7ab27e4963d
iiwaasnet <givasena@gmail.com>
2016-05-25 22:28:31 +0200

    Updated wire format version

--- src/kino.Core/Messaging/Versioning.cs
+++ src/kino.Core/Messaging/Versioning.cs
@@ -2,9 +2,7 @@
 {
     public class Versioning
     {
-        public const int WireFormatV1 = 1;
-        public const int WireFormatV2 = 2;
-        public const int WireFormatV3 = 3;
-        public const int CurrentWireFormatVersion = WireFormatV3;
+        public const int WireFormatV4 = 4;
+        public const int CurrentWireFormatVersion = WireFormatV4;
     }
 }
