387547686aa0c0e207bba546352479175c81fe36
iiwaasnet <givasena@gmail.com>
2016-06-05 20:02:47 +0200

    Added chocolatey spec and pack script

---
+++ NuGetPackages/pack.choco.ps1
@@ -0,0 +1,18 @@
+$MsBuild = "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe"
+
+$SolutionFile = Get-ChildItem ..\src -Recurse | Where-Object {$_.Extension -eq ".sln"}
+nuget restore $SolutionFile.FullName
+
+foreach ($NugetSpec in Get-ChildItem ..\src -Recurse | Where-Object {$_.Extension -eq ".nuspec"})
+{
+	$ProjectFile = Get-ChildItem $NugetSpec.DirectoryName | Where-Object {$_.Extension -eq ".csproj"}
+
+	$BuildArgs = @{
+		FilePath = $MsBuild
+		ArgumentList = $ProjectFile.FullName,  "/p:Configuration=Release /t:Rebuild"
+		Wait = $true
+	}
+	
+	Start-Process @BuildArgs -NoNewWindow
+	cpack $NugetSpec.FullName
+}
