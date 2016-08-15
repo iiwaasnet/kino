7ca75e2583c128cb2b9a23bde0bbf39df211671c
iiwaasnet <givasena@gmail.com>
2016-08-15 15:31:30 +0200

    Unittests

---
+++ src/Tests/kino.Tests/Connectivity/NodeMessageRoutesRequestHandlerTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using kino.Core.Connectivity;
+using kino.Core.Connectivity.ServiceMessageHandlers;
+using kino.Core.Messaging;
+using kino.Core.Messaging.Messages;
+using kino.Core.Security;
+using kino.Core.Sockets;
+using kino.Tests.Actors.Setup;
+using Moq;
+using NUnit.Framework;
+
+namespace kino.Tests.Connectivity
+{
+    [TestFixture]
+    public class NodeMessageRoutesRequestHandlerTests
+    {
+        private string domain;
+        private Mock<ISecurityProvider> securityProvider;
+        private NodeMessageRoutesRequestHandler handler;
+        private Mock<IClusterMonitor> clusterMonitor;
+        private Mock<IClusterMonitorProvider> clusterMonitorProvider;
+        private Mock<IInternalRoutingTable> internalRoutingTable;
+        private Mock<ISocket> socket;
+
+        [SetUp]
+        public void Setup()
+        {
+            socket = new Mock<ISocket>();
+            domain = Guid.NewGuid().ToString();
+            securityProvider = new Mock<ISecurityProvider>();
+            securityProvider.Setup(m => m.DomainIsAllowed(domain)).Returns(true);
+            securityProvider.Setup(m => m.DomainIsAllowed(It.Is<string>(d => d != domain))).Returns(false);
+            securityProvider.Setup(m => m.GetAllowedDomains()).Returns(new[] {domain});
+            securityProvider.As<ISignatureProvider>().Setup(m => m.CreateSignature(domain, It.IsAny<byte[]>())).Returns(new byte[0]);
+            securityProvider.As<ISignatureProvider>().Setup(m => m.CreateSignature(It.Is<string>(d => d != domain), It.IsAny<byte[]>())).Throws<SecurityException>();
+            clusterMonitor = new Mock<IClusterMonitor>();
+            clusterMonitorProvider = new Mock<IClusterMonitorProvider>();
+            clusterMonitorProvider.Setup(m => m.GetClusterMonitor()).Returns(clusterMonitor.Object);
+            internalRoutingTable = new Mock<IInternalRoutingTable>();
+            handler = new NodeMessageRoutesRequestHandler(clusterMonitorProvider.Object,
+                                                          internalRoutingTable.Object,
+                                                          securityProvider.Object);
+        }
+
+        [Test]
+        public void IfDomainIsNotAllowed_SelfRegistrationIsNotSent()
+        {
+            var domain = Guid.NewGuid().ToString();
+            var message = Message.Create(new RequestNodeMessageRoutesMessage(), domain);
+            //
+            handler.Handle(message, socket.Object);
+            //
+            internalRoutingTable.Verify(m => m.GetMessageIdentifiers(), Times.Never);
+            clusterMonitor.Verify(m => m.RegisterSelf(It.IsAny<IEnumerable<MessageIdentifier>>(), domain), Times.Never);
+        }
+
+        [Test]
+        public void IfForRequestedDomainNoMessageHandlersRegistered_SelfRegistrationIsNotSent()
+        {
+            var otherDomain = Guid.NewGuid().ToString();
+            var messageIdentifier = MessageIdentifier.Create<SimpleMessage>();
+            internalRoutingTable.Setup(m => m.GetMessageIdentifiers()).Returns(new[] {messageIdentifier});
+            securityProvider.Setup(m => m.GetDomain(messageIdentifier.Identity)).Returns(otherDomain);
+            var message = Message.Create(new RequestNodeMessageRoutesMessage(), domain);
+            //
+            handler.Handle(message, socket.Object);
+            //
+            internalRoutingTable.Verify(m => m.GetMessageIdentifiers(), Times.Once);
+            clusterMonitor.Verify(m => m.RegisterSelf(It.IsAny<IEnumerable<MessageIdentifier>>(), domain), Times.Never);
+        }
+
+        [Test]
+        public void IfForRequestedDomainNoMessageHandlersRegistered_SelfRegistrationIsSentOnlyForMessageHubs()
+        {
+            var otherDomain = Guid.NewGuid().ToString();
+            var messageHandler = MessageIdentifier.Create<SimpleMessage>();
+            var messageHub = new MessageIdentifier(Guid.NewGuid().ToByteArray());
+            internalRoutingTable.Setup(m => m.GetMessageIdentifiers()).Returns(new[] {messageHandler, messageHub});
+            securityProvider.Setup(m => m.GetDomain(messageHandler.Identity)).Returns(otherDomain);
+            var message = Message.Create(new RequestNodeMessageRoutesMessage(), domain);
+            //
+            handler.Handle(message, socket.Object);
+            //
+            internalRoutingTable.Verify(m => m.GetMessageIdentifiers(), Times.Once);
+            clusterMonitor.Verify(m => m.RegisterSelf(It.Is<IEnumerable<MessageIdentifier>>(ids => ids.SequenceEqual(new[] {messageHub})),
+                                                      domain),
+                                  Times.Once);
+        }
+    }
+}
---
+++ src/Tests/kino.Tests/Connectivity/UnreachableNodeUnregistrationHandlerTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using kino.Core.Connectivity;
+using kino.Core.Connectivity.ServiceMessageHandlers;
+using kino.Core.Messaging;
+using kino.Core.Messaging.Messages;
+using kino.Core.Sockets;
+using Moq;
+using NUnit.Framework;
+
+namespace kino.Tests.Connectivity
+{
+    [TestFixture]
+    public class UnreachableNodeUnregistrationHandlerTests
+    {
+        private Mock<IClusterMembership> clusterMembership;
+        private Mock<IExternalRoutingTable> externalRoutingTable;
+        private UnreachableNodeUnregistrationHandler handler;
+        private Mock<ISocket> socket;
+
+        [SetUp]
+        public void Setup()
+        {
+            socket = new Mock<ISocket>();
+            clusterMembership = new Mock<IClusterMembership>();
+            externalRoutingTable = new Mock<IExternalRoutingTable>();
+            handler = new UnreachableNodeUnregistrationHandler(externalRoutingTable.Object, clusterMembership.Object);
+        }
+
+        [Test]
+        [TestCase(true)]
+        [TestCase(false)]
+        public void InregisterUnreachableNode_DeletesClusterMemberRemovesNodeFromExternalRoutingTableAndDisconnectsIt(bool disconnect)
+        {
+            var socketIdentity = Guid.NewGuid().ToByteArray();
+            var uri = "tcp://localhost";
+            externalRoutingTable.Setup(m => m.RemoveNodeRoute(new SocketIdentifier(socketIdentity)))
+                                .Returns(disconnect
+                                             ? PeerConnectionAction.Disconnect
+                                             : PeerConnectionAction.None);
+            var message = Message.Create(new UnregisterUnreachableNodeMessage
+                                         {
+                                             Uri = uri,
+                                             SocketIdentity = socketIdentity
+                                         },
+                                         string.Empty);
+            //
+            handler.Handle(message, socket.Object);
+            //
+            clusterMembership.Verify(m => m.DeleteClusterMember(new SocketEndpoint(new Uri(uri), socketIdentity)), Times.Once);
+            externalRoutingTable.Verify(m => m.RemoveNodeRoute(new SocketIdentifier(socketIdentity)), Times.Once);
+            socket.Verify(m => m.Disconnect(new Uri(uri)), Times.Exactly(disconnect ? 1 : 0));
+        }
+    }
+}
