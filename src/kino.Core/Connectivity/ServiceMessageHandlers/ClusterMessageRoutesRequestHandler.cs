78ec24c5aa36b0656fa732444584f4bebba998fa
iiwaasnet <givasena@gmail.com>
2016-08-15 16:42:10 +0200

    Unittests

--- src/kino.Core/Connectivity/ServiceMessageHandlers/ClusterMessageRoutesRequestHandler.cs
+++ src/kino.Core/Connectivity/ServiceMessageHandlers/ClusterMessageRoutesRequestHandler.cs
@@ -32,8 +32,7 @@ namespace kino.Core.Connectivity.ServiceMessageHandlers
                     message.As<Message>().VerifySignature(securityProvider);
 
                     var messageIdentifiers = internalRoutingTable.GetMessageIdentifiers();
-                    var messageHubs = messageIdentifiers.Where(mi => mi.IsMessageHub())
-                                                        .ToList();
+                    var messageHubs = messageIdentifiers.Where(mi => mi.IsMessageHub());
                     //TODO: Refactor, hence !mi.IsMessageHub() should be first condition
                     var contracts = messageIdentifiers.Where(mi => !mi.IsMessageHub() &&
                                                                    securityProvider.GetDomain(mi.Identity) == message.Domain)
