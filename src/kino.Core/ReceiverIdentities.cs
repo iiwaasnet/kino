035dd1e827624ed12e1b5f9c77431215f3718d43
iiwaasnet <givasena@gmail.com>
2016-12-14 20:42:11 +0100

    BUGFIX: MessageHub/Actor identification

--- src/kino.Core/ReceiverIdentities.cs
+++ src/kino.Core/ReceiverIdentities.cs
@@ -17,16 +17,20 @@ namespace kino.Core
             => new ReceiverIdentifier(new byte[] {MessageHubSignature}.Concat(ReceiverIdentifier.CreateIdentity()).ToArray());
 
         public static bool IsActor(this ReceiverIdentifier identifier)
-            => identifier?.Identity?.FirstOrDefault() == ActorSignature;
+            => identifier?.Identity?.Length > 0
+               && identifier.Identity[0] == ActorSignature;
 
-        public static bool IsActor(this IEnumerable<byte> identifier)
-            => identifier?.FirstOrDefault() == ActorSignature;
+        public static bool IsActor(this IList<byte> identifier)
+            => identifier?.Count > 0
+               && identifier[0] == ActorSignature;
 
         public static bool IsMessageHub(this ReceiverIdentifier identifier)
-            => identifier?.Identity?.FirstOrDefault() == MessageHubSignature;
+            => identifier?.Identity?.Length > 0
+               && identifier.Identity[0] == MessageHubSignature;
 
-        public static bool IsMessageHub(this IEnumerable<byte> identifier)
-            => identifier?.FirstOrDefault() == MessageHubSignature;
+        public static bool IsMessageHub(this IList<byte> identifier)
+            => identifier?.Count > 0
+               && identifier[0] == MessageHubSignature;
 
         public static bool IsSet(this ReceiverIdentifier identitifier)
             => identitifier?.Identity != null && !Unsafe.ArraysEqual(identitifier.Identity, Empty);
