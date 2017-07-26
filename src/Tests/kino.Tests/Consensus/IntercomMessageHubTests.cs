e80ea727ad4ba7e8de70b25cd4a8f54d70ed5339
iiwaasnet <givasena@gmail.com>
2017-07-26 20:25:31 +0200

    Consensus heartbeating

--- src/Tests/kino.Tests/Consensus/IntercomMessageHubTests.cs
+++ src/Tests/kino.Tests/Consensus/IntercomMessageHubTests.cs
@@ -10,6 +10,7 @@ using kino.Core.Diagnostics;
 using kino.Core.Diagnostics.Performance;
 using kino.Core.Framework;
 using kino.Messaging;
+using kino.Tests.Actors.Setup;
 using kino.Tests.Helpers;
 using Moq;
 using NUnit.Framework;
@@ -101,17 +102,92 @@ namespace kino.Tests.Consensus
         public void IfReconnectMessageArrives_ConnectionToNodeIsReestablished()
         {
             var messageCount = 1;
-            var deadNode = messageHub.GetClusterHealthInfo().First().NodeUri;
-            var message = Message.Create(new ReconnectClusterMemberMessage {NodeUri = deadNode.ToSocketAddress()});
+            var deadNode = messageHub.GetClusterHealthInfo().First();
+            var message = Message.Create(new ReconnectClusterMemberMessage {NodeUri = deadNode.NodeUri.ToSocketAddress()});
             subscriberSocket.Setup(m => m.ReceiveMessage(It.IsAny<CancellationToken>())).Returns(() => messageCount-- > 0 ? message : null);
             //
             var timeout = TimeSpan.FromSeconds(3);
+            var now = DateTime.UtcNow;
             messageHub.Start(timeout);
             timeout.Sleep();
             messageHub.Stop();
             //
-            subscriberSocket.Verify(m => m.Disconnect(deadNode), Times.Once);
-            subscriberSocket.Verify(m => m.Connect(deadNode, false), Times.Once);
+            subscriberSocket.Verify(m => m.Disconnect(deadNode.NodeUri), Times.Once);
+            subscriberSocket.Verify(m => m.Connect(deadNode.NodeUri, false), Times.Once);
+            Assert.LessOrEqual(now, deadNode.LastKnownHeartBeat);
+        }
+
+        [Test]
+        public void WhenHeartBeatMessageArrives_LastKnownHeartBeatIsUpdated()
+        {
+            synodConfig.Setup(m => m.MissingHeartBeatsBeforeReconnect).Returns(1);
+            messageHub = new IntercomMessageHub(socketFactory.Object,
+                                                synodConfig.Object,
+                                                perfCounterManager.Object,
+                                                logger.Object);
+            var messageCount = 1;
+            var deadNode = messageHub.GetClusterHealthInfo().First();
+            var message = Message.Create(new HeartBeatMessage {NodeUri = deadNode.NodeUri.ToSocketAddress()});
+            subscriberSocket.Setup(m => m.ReceiveMessage(It.IsAny<CancellationToken>())).Returns(() => messageCount-- > 0 ? message : null);
+
+            synodConfig.Object
+                       .HeartBeatInterval
+                       .MultiplyBy(synodConfig.Object.MissingHeartBeatsBeforeReconnect + 1)
+                       .Sleep();
+            var timeout = synodConfig.Object
+                                     .HeartBeatInterval
+                                     .DivideBy(2);
+            //
+            Assert.IsFalse(deadNode.IsHealthy());
+            messageHub.Start(timeout);
+            timeout.Sleep();
+            messageHub.Stop();
+            //
+            Assert.IsTrue(deadNode.IsHealthy());
+        }
+
+        [Test]
+        public void WhenMessageArrives_SubscriberIsNotified()
+        {
+            var messageCount = 1;
+            var message = Message.Create(new SimpleMessage());
+            subscriberSocket.Setup(m => m.ReceiveMessage(It.IsAny<CancellationToken>())).Returns(() => messageCount-- > 0 ? message : null);
+            var observer = new Mock<IObserver<IMessage>>();
+            messageHub.Subscribe().Subscribe(observer.Object);
+            //
+            var timeout = TimeSpan.FromSeconds(2);
+            messageHub.Start(timeout);
+            timeout.Sleep();
+            messageHub.Stop();
+            //
+            observer.Verify(m => m.OnNext(message), Times.Once);
+        }
+
+        [Test]
+        public void Broadcast_SendsMessageToAllSubscribers()
+        {
+            var message = Message.Create(new SimpleMessage()).As<Message>();
+            var timeout = TimeSpan.FromSeconds(1);
+            messageHub.Start(timeout);
+            messageHub.Broadcast(message);
+            timeout.Sleep();
+            messageHub.Stop();
+            //
+            Assert.IsTrue(Unsafe.ArraysEqual(new byte[0], message.SocketIdentity));
+        }
+
+        [Test]
+        public void Send_SendsMessageToOneSubscribers()
+        {
+            var message = Message.Create(new SimpleMessage()).As<Message>();
+            var receiver = ReceiverIdentifier.CreateIdentity();
+            var timeout = TimeSpan.FromSeconds(1);
+            messageHub.Start(timeout);
+            messageHub.Send(message, receiver);
+            timeout.Sleep();
+            messageHub.Stop();
+            //
+            Assert.IsTrue(Unsafe.ArraysEqual(receiver, message.SocketIdentity));
         }
     }
 }
