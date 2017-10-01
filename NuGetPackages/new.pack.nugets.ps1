27ed11f1e3f0dee54e1de75ac28a5551e5b3a044
iiwaasnet <givasena@gmail.com>
2017-10-01 20:30:19 +0200

    NuGet build script

--- NuGetPackages/new.pack.nugets.ps1
+++ NuGetPackages/new.pack.nugets.ps1
@@ -4,14 +4,22 @@ function Get-NuGetDependencies([string]$projectFile, [string]$basePath)
     $projectFile = [IO.Path]::GetFullPath([IO.Path]::Combine($basePath,$projectFile))
 
     [xml] $projectXml = Get-Content ($projectFile)
+
+    $tmp = $projectXml.SelectSingleNode('//ItemGroup/PackageReference')
+    Write-Host $tmp.InnerText
+
     foreach ($package in $projectXml.Project.ItemGroup.PackageReference)
     {
-        $nugetRef = New-Object System.Object
-        $nugetRef | Add-Member -type NoteProperty -name Name -value $package.Include
-        $nugetRef | Add-Member -type NoteProperty -name Version -value $package.Version
+        if ($package.Include)
+        {
+            $nugetRef = New-Object System.Object
+            $nugetRef | Add-Member -type NoteProperty -name Name -value $package.Include
+            $nugetRef | Add-Member -type NoteProperty -name Version -value $package.Version
 
-        $nugetRefs += $nugetRef
+            $nugetRefs += $nugetRef
+        }
     }
+    
 
     foreach ($ref in $projectXml.Project.ItemGroup.ProjectReference.Include)
     {
@@ -51,6 +59,81 @@ function Should-BuildNuGetPackage([xml]$projectXml)
     return $projectXml.SelectSingleNode('//PropertyGroup/BuildNuGetPackage')
 }
 
+function Get-NuGetTargetPlatform([string]$fw)
+{
+    if ($fw -eq 'netstandard2.0')
+    {
+        return '.NETStandard2.0'
+    }
+    if ($fw -eq 'net47')
+    {
+        return '.NETFramework4.7'
+    }
+
+    throw 'Framework is not supported: ' + $fw
+}
+
+function Copy-ProjectNuGetAttributes([xml]$projectXml, [System.Xml.XmlNode]$meta, [System.Xml.XmlDocument]$doc)
+{
+    foreach ($element in (Get-ProjectNuGetAttributesMap))
+    {
+        $node = $doc.CreateElement($element.Destination, (Get-XmlNs))
+        $node.InnerText = $projectXml.SelectSingleNode('//PropertyGroup/' + $element.Source).InnerText
+        $meta.AppendChild($node)
+    }
+}
+
+function Get-ProjectNuGetAttributesMap()
+{
+    $attrs = @()
+    $attr = New-Object System.Object
+    $attr | Add-Member -type NoteProperty -name Source -value 'Copyright'
+    $attr | Add-Member -type NoteProperty -name Destination -value 'copyright'
+    $attrs += $attr
+
+    $attr = New-Object System.Object
+    $attr | Add-Member -type NoteProperty -name Source -value 'PackageLicenseUrl'
+    $attr | Add-Member -type NoteProperty -name Destination -value 'licenseUrl'
+    $attrs += $attr
+
+    $attr = New-Object System.Object
+    $attr | Add-Member -type NoteProperty -name Source -value 'PackageProjectUrl'
+    $attr | Add-Member -type NoteProperty -name Destination -value 'projectUrl'
+    $attrs += $attr
+
+    $attr = New-Object System.Object
+    $attr | Add-Member -type NoteProperty -name Source -value 'PackageIconUrl'
+    $attr | Add-Member -type NoteProperty -name Destination -value 'iconUrl'
+    $attrs += $attr
+
+    $attr = New-Object System.Object
+    $attr | Add-Member -type NoteProperty -name Source -value 'Description'
+    $attr | Add-Member -type NoteProperty -name Destination -value 'description'
+    $attrs += $attr
+
+    $attr = New-Object System.Object
+    $attr | Add-Member -type NoteProperty -name Source -value 'PackageTags'
+    $attr | Add-Member -type NoteProperty -name Destination -value 'tags'
+    $attrs += $attr
+
+    $attr = New-Object System.Object
+    $attr | Add-Member -type NoteProperty -name Source -value 'Authors'
+    $attr | Add-Member -type NoteProperty -name Destination -value 'authors'
+    $attrs += $attr
+
+    $attr = New-Object System.Object
+    $attr | Add-Member -type NoteProperty -name Source -value 'Authors'
+    $attr | Add-Member -type NoteProperty -name Destination -value 'owners'
+    $attrs += $attr
+
+    return $attrs
+}
+
+function Get-XmlNs()
+{
+    return 'http://schemas.microsoft.com/packaging/2013/05/nuspec.xsd'
+}
+
 $solutionFile = Get-ChildItem ..\src -Recurse | Where-Object {$_.Extension -eq ".sln"}
 
 foreach ($projectFile in Get-ChildItem ..\src -Recurse | Where-Object {$_.Extension -eq ".csproj"})
@@ -87,20 +170,21 @@ foreach ($projectFile in Get-ChildItem ..\src -Recurse | Where-Object {$_.Extens
         #Build NuSpec file
         [xml]$nuSpec = New-Object System.Xml.XmlDocument
         $nuSpec.AppendChild($nuSpec.CreateXmlDeclaration("1.0","UTF-8",$null))
-        $root = $nuSpec.CreateNode('element', 'package', $null)
-        $meta = $nuSpec.CreateNode('element', 'metadata', $root)
-        $meta.SetAttribute('xmlns', 'http://schemas.microsoft.com/packaging/2013/05/nuspec.xsd')
-        $nuSpec.AppendChild($meta)
-        $dependencies = $nuSpec.CreateNode('element', 'dependencies', $null)
+        $root = $nuSpec.CreateElement('package', (Get-XmlNs))
+        $nuSpec.AppendChild($root)
+        $meta = $nuSpec.CreateElement('metadata', (Get-XmlNs))        
+        $root.AppendChild($meta)
+        (Copy-ProjectNuGetAttributes $projectXml $meta $nuSpec)
+        $dependencies = $nuSpec.CreateElement('dependencies', (Get-XmlNs))
         $meta.AppendChild($dependencies)
         foreach ($fw in $frameworks)
         {
             Write-Host $fw -ForegroundColor Yellow
-            $grp = $nuSpec.CreateNode('element', 'group', $null)
-            $grp.SetAttribute('targetFramework', $fw)
+            $grp = $nuSpec.CreateElement('group', (Get-XmlNs))
+            $grp.SetAttribute('targetFramework', (Get-NuGetTargetPlatform $fw))
             foreach ($nugetRef in $nugetRefs)
             {
-                $ref = $nuSpec.CreateNode('element', 'dependency', $null)
+                $ref = $nuSpec.CreateElement('dependency', (Get-XmlNs))
                 $ref.SetAttribute('id', $nugetRef.Name)
                 $ref.SetAttribute('version', $nugetRef.Version)
                 $grp.AppendChild($ref)
