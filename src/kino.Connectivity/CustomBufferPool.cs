5056166debd36b4fd05f449913412e8d988ffc0d
iiwaasnet <givasena@gmail.com>
2018-10-27 11:28:44 +0200

    Custom buffer pool for NetMQ

---
+++ src/kino.Connectivity/CustomBufferPool.cs
@@ -0,0 +1,18 @@
+﻿using System.Buffers;
+using NetMQ;
+
+namespace kino.Connectivity
+{
+    public class CustomBufferPool : IBufferPool
+    {
+        public void Dispose()
+        {
+        }
+
+        public byte[] Take(int size)
+            => ArrayPool<byte>.Shared.Rent(size);
+
+        public void Return(byte[] buffer)
+            => ArrayPool<byte>.Shared.Return(buffer, true);
+    }
+}
